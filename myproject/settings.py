e937812540cc4bbbea364dbd0f8aad8befb1545a
santosh <santoshpaudyal99@gmail.com>
2019-04-27 12:34:49 +0545

    front-end,url routing and signup

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'widget_tweaks',
+
+    'accounts',
+
     'boards',
 ]
 
@@ -127,3 +131,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGOUT_REDIRECT_URL = 'home'
